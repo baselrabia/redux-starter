aa1257c8ef2820d70de1a0c7cc982df13a7d7a9d
baselrabia <baselrabia@gmail.com>
2020-08-23 06:19:03 +0200

    4-composing and piping

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// Functions Composition
+// composing and piping
 
-
+import {compose,pipe} from 'lodash/fp';
 
 let input = "   javascript  ";
 let output = "<div>" + input.trim() + "</div>";
@@ -15,4 +15,9 @@ const trim = str => str.trim();
 const wrapInDev = str => `<div> ${str} </div>`;
 const toLowerCase = (str) => str.toLowerCase();
 
-const result = wrapInDev(toLowerCase(trim(input)));
+const transform = compose(wrapInDev, toLowerCase, trim);
+// transform2  give same result but diffent is the sort of the functions arguments => easy to read 
+const transform2 = pipe(trim, toLowerCase, wrapInDev);
+
+//const result = wrapInDev(toLowerCase(trim(input)));
+const result = transform(input);
