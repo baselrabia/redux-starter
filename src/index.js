8d87142a74306e2f4daaf50fdef0414b90d7f01b
baselrabia <baselrabia@gmail.com>
2020-08-23 10:58:27 +0200

    19-subscribing to the store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-//  despatcing actions
+//  subscribing to the store 
 import store from './store';
+
+// it called every time the state of the store got changed 
+const unsubscribe = store.subscribe( () => {
+    console.log("store changed!", store.getState());
+})
+// if you are using vanile js or jquery so when change it will refresh the view 
+//but in redux and react it Rerender the view 
+// ui elements just subscribed to the store 
+//so they get notified when the state changed 
 
 // action should have 2 props
 store.dispatch({
@@ -9,6 +18,12 @@ store.dispatch({
     }
 });
 
+unsubscribe();
+
+//unsubscribe the ui elements  
+// if we are in new page and ui elements not gonna be vaisable 
+// we should unsubscribe them from the store 
+
 store.dispatch({
   type: "bugRemoved",
   payload: {
@@ -16,4 +31,4 @@ store.dispatch({
   },
 });
 
-console.log(store.getState());
+
