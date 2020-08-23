6b9bf77b0cf4f85fba9df21fb052bb350d638058
baselrabia <baselrabia@gmail.com>
2020-08-23 10:09:44 +0200

    15-defining the actions

--- src/index.js
+++ src/index.js
@@ -1,38 +1,24 @@
-//  designing the store 
+//  defining the actions
 
-//store design for bugs so u will nedd bugs reducer 
-//we focous here on single slice and the simple structure 
-//that allow us to study redux 
-//and see all it's build-in bugs in action 
+// add a bug 
+// mark as resolved 
+// delete a bug 
 
-[
-  {
-    id: 1,
-    description: "",
-    resolved: false,
-  },
-  {
-    id: 2,
-    description: "",
-    resolved: false,
-  },
-  {
-    id: 3,
-    description: "",
-    resolved: false,
-  },
-
-];
-
-// in real app
-// u can refactor this structre to something like that 
 {
-    bugs: [
-        {
-            id: 1,
-            description: "",
-            resolved: false
-        }
-    ],
-    currentUser: {}
+    type: "bugAdded",
+    payload: {
+         description:"..."
+    }
+   
 }
+
+{
+    type: "bugRemoved",
+    payload: {
+         id: 1
+    }
+   
+}
+
+// type is the name of action 
+// payload is the data that u want to change in reducer
