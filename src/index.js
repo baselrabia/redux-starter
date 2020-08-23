f540a2b943b73746f183558f198ef225597550cd
baselrabia <baselrabia@gmail.com>
2020-08-23 06:40:19 +0200

    6-pure functions

--- src/index.js
+++ src/index.js
@@ -1,41 +1,16 @@
-// curring
-// is to convert fun which takes 2 argumets and return same function but take 1 argument
-// by passing the seacond argument at first 
+// pure functions
+// same args => same result 
 
-function add(a) {
-    return function(b) {
-        return a+b;
-    };
-}
-const add1 = add(1);
-add1(5); 
+// no random values
+// no current data/time 
+// no global state ( Dom, Files ,DB , etc)
+// no mutation of parmeters
 
-add(1)(5);
-const add2 = a => b => a + b ; // (a,b) => a + b 
-add2(2)(5)
-import {compose,pipe} from 'lodash/fp';
+// our redux reducer should be pure funs
 
-let input = "   javascript  ";
-let output = "<div>" + input.trim() + "</div>";
+// #Benefits 
 
-// reusable functions 
-
-//trim 
-//wrapInDev
-//toLowerCase
-
-const trim = str => str.trim();
-// const wrapInDev = str => `<div> ${str} </div>`;
-// const wrapInSpan = (str) => `<span> ${str} </span>`;
-//const wrap = (type, str) => `<${type}> ${str} </${type}>`;
-
-//convert wrap to curring function
-
-const wrap = type => str => `<${type}> ${str} </${type}>`;
-
-const toLowerCase = (str) => str.toLowerCase();
-
-const transform = pipe(trim, toLowerCase, wrap("dev"));
-
-//const result = wrapInDev(toLowerCase(trim(input)));
-const result = transform(input);
+// 1- self-documenting
+// 2- easily testable
+// 3- concurrency
+// 4- cacheable
