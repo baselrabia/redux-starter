ae3ac4f205dc8e98845565d3f2095d443ab39725
baselrabia <baselrabia@gmail.com>
2020-08-23 05:55:08 +0200

    1-Function as first-class citizens

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+// Function as first-class citizens
+
+// - assign function as a variable 
+function sayHello() {
+    return "hello world" 
+}
+
+let fn = sayHello;
+fn()
+sayHello()
+
+// - pass function as an argument
+
+function greet(fnMessage) {
+    console.log(fnMessage())
+}
+
+greet(sayHello);
+
+// - return function from other function 
+function sayHello() {
+  return function () {
+     return "hello world"
+  }
+}
+
+let fn = sayHello()
+let message = fn()
