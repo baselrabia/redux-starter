2ce1cada5202978965370af98c1a2f22035dcf71
baselrabia <baselrabia@gmail.com>
2020-08-23 09:48:33 +0200

    13-first redux app -- setup steps

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-//  redux architecture
-// use functional programming 
-// dont use mutuable state 
-// cant write code like !=  =>  store.currentUser = {name:"mosh"}
+//  first redux app
 
-//to update store we use reducer 
-// reducer {event handler}
+// setup steps 
 
-// store => is your state mangement by redux 
-
-//actions {event} => the functions that u wanna do on your store to update it 
-// it describe  {what u wanna do}  OR  {what just happened} ex. user.LogedIn, logedOut
-
-
-//store has dispatch method to take action and forward to reducer 
-// reducer handel the state and return changes to store 
-//then store change the state internally ,
-// ui componets takes the data and refresh themselves
-//we dispatch actions in one entrance to control 
-//what should happen when user perfome an action
-// so we can log every actions and how the state change
-
-function reducer(store , action) {
-    // return updated store 
-    const updated = {...store};
-}
+// 1- design the store 
+// 2- define the action -- what actions can user preform throw the app 
+// 3- create a reducer -- take action then return updated state
+// 4- set up the store  -- base on your reducer
