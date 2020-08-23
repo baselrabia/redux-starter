692adb5d82e8ebd3765f99f1db1e5b3582e74706
baselrabia <baselrabia@gmail.com>
2020-08-23 06:32:46 +0200

    5-curring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-// composing and piping
+// curring
+// is to convert fun which takes 2 argumets and return same function but take 1 argument
+// by passing the seacond argument at first 
 
+function add(a) {
+    return function(b) {
+        return a+b;
+    };
+}
+const add1 = add(1);
+add1(5); 
+
+add(1)(5);
+const add2 = a => b => a + b ; // (a,b) => a + b 
+add2(2)(5)
 import {compose,pipe} from 'lodash/fp';
 
 let input = "   javascript  ";
@@ -12,12 +25,17 @@ let output = "<div>" + input.trim() + "</div>";
 //toLowerCase
 
 const trim = str => str.trim();
-const wrapInDev = str => `<div> ${str} </div>`;
+// const wrapInDev = str => `<div> ${str} </div>`;
+// const wrapInSpan = (str) => `<span> ${str} </span>`;
+//const wrap = (type, str) => `<${type}> ${str} </${type}>`;
+
+//convert wrap to curring function
+
+const wrap = type => str => `<${type}> ${str} </${type}>`;
+
 const toLowerCase = (str) => str.toLowerCase();
 
-const transform = compose(wrapInDev, toLowerCase, trim);
-// transform2  give same result but diffent is the sort of the functions arguments => easy to read 
-const transform2 = pipe(trim, toLowerCase, wrapInDev);
+const transform = pipe(trim, toLowerCase, wrap("dev"));
 
 //const result = wrapInDev(toLowerCase(trim(input)));
 const result = transform(input);
